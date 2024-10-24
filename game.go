31eb4dabc76f3faf02c2f85fb78f51b7b0181cc0
Adrian Eliasson <adrian.eliasson@hotmail.com>
2024-10-24 23:14:44 +0200

    simplify code

--- game.go
+++ game.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"slices"
 	"bufio"
 	"fmt"
 	"os"
@@ -24,44 +25,20 @@ func (g *Game) init() {
 	g.playerTurn = 1
 }
 
-func xoro(n int) string {
-	if n == 0 {
-		return "_"
-	} else if n == 1 {
-		return "X"
-	} else if n == 2 {
-		return "O"
-	} else {
-		return "WRONG"
-	}
-}
-
-func count[T any](slice []T, f func(T) bool) int {
-	count := 0
-	for _, s := range slice {
-		if f(s) {
-			count++
-		}
-	}
-	return count
-}
-
-func countOnesOrTwos(slice []int) int {
-	return count(slice, func(x int) bool { return x == 1 || x == 2 })
-}
-
 func (g *Game) placePiece(colNumber int) {
-	numberOfNonZeroes := countOnesOrTwos(g.board[colNumber])
-	if len(g.board[colNumber]) < numberOfNonZeroes+1 {
+	firstEmptySlot := slices.IndexFunc(g.board[colNumber], func(n int) bool { return n == 0 })
+	hasEmptySlots := firstEmptySlot < 0
+	if hasEmptySlots {
 		fmt.Println("That column is full")
 		return
 	}
-	g.board[colNumber][numberOfNonZeroes] = g.playerTurn
+	g.board[colNumber][firstEmptySlot] = g.playerTurn
 	g.changeTurn()
 }
 
 func (g *Game) play() {
 	for !g.isOver() {
+		g.printGameState()
 		g.makeTurn()
 	}
 }
@@ -71,11 +48,22 @@ func (g Game) isOver() bool {
 }
 
 func (g *Game) changeTurn() {
-	fmt.Println("Current: ", g.playerTurn)
 	if g.playerTurn == 1 {
 		g.playerTurn = 2
 	} else {
 		g.playerTurn = 1
+	}
+}
+
+func xoro(n int) string {
+	if n == 0 {
+		return "_"
+	} else if n == 1 {
+		return "X"
+	} else if n == 2 {
+		return "O"
+	} else {
+		return "WRONG"
 	}
 }
 
@@ -89,7 +77,6 @@ func (g Game) printGameState() {
 }
 
 func (g *Game) makeTurn() {
-	g.printGameState()
 	fmt.Println("Player ", g.playerTurn, "'s turn")
 	fmt.Print("Which column do you put your piece? (or type \"quit\" to quit) [1,6]: ")
 	reader := bufio.NewReader(os.Stdin)
